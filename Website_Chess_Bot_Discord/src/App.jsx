6fa870adafae54449f3c067431bd4d78b00adcab
Victor Pereira <victorgamedeveloper@gmail.com>
2023-03-30 10:00:05 -0300

    03/29/2023
    03/29/2023

--- Website_Chess_Bot_Discord/src/App.jsx
+++ Website_Chess_Bot_Discord/src/App.jsx
@@ -9,39 +9,39 @@ function App() {
         <img src={Logo} />
         <h1>Chess Bot</h1>
         <p>Um bot de xadrez para seu servidor discord</p>
-        <button>Convite</button>
+        <a href='https://discord.com/oauth2/authorize?client_id=1077947825595351190&permissions=8&scope=bot' target="_blank">Convite</a>
       </div>
       <div className="Bottom">
         <h1>Comandos</h1>
         <div className="Container">
           <div className="Left">
             <div className="Command">
-              <h1>/chess</h1>
+              <h1>/multiplayer</h1>
               <p>Criar partida multiplayer</p>
             </div>
             <div className="Command">
-              <h1>/ai</h1>
+              <h1>/singleplayer</h1>
               <p>Criar partida contra IA</p>
             </div>
             <div className="Command">
-              <h1>/move</h1>
+              <h1>/mover</h1>
               <p>Mover uma peça</p>
             </div>
             <div className="Command">
-              <h1>/restart</h1>
+              <h1>/reiniciar</h1>
               <p>Reiniciar a partida</p>
             </div>
             <div className="Command">
-              <h1>/recover</h1>
+              <h1>/recuperar</h1>
               <p>Recuperar partida em andamento</p>
             </div>
             <div className="Command">
-              <h1>/delete</h1>
-              <p>Finaliza a partida</p>
+              <h1>/finalizar</h1>
+              <p>Termine a partida</p>
             </div>
             <div className="Command">
-              <h1>/commands</h1>
-              <p>Lista todos os comandos</p>
+              <h1>/comandos</h1>
+              <p>Lista todos os comandos</p>
             </div>
           </div>
           <div className="Right">
